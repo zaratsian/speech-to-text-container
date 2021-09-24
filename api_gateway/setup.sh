7e13388aaf4a1257b94fb082a05ac866828e2660
Dan Zaratsian <dtz001@gmail.com>
2021-09-24 18:44:54 +0000

    added api gateway

--- api_gateway/setup.sh
+++ api_gateway/setup.sh
@@ -3,7 +3,6 @@ API_REGION="us-east1"
 APP_ID=google-toxicity
 CONFIG_ID=${APP_ID}-config
 GATEWAY_ID=${APP_ID}-gateway
-#BACKEND_SERVICE_ACCOUNT=service-826671308632@serverless-robot-prod.iam.gserviceaccount.com
 
 # Enable GCP APIs
 gcloud services enable apigateway.googleapis.com
@@ -13,7 +12,7 @@ gcloud services enable servicecontrol.googleapis.com
 # Create an API Config
 gcloud api-gateway api-configs create ${CONFIG_ID} \
   --api=${APP_ID} --openapi-spec=openapi2-run.yaml \
-  --project=${GCP_PROJECT_ID} #--backend-auth-service-account=${BACKEND_SERVICE_ACCOUNT}
+  --project=${GCP_PROJECT_ID}
 
 # Deploy API Gateway
 gcloud api-gateway gateways create ${GATEWAY_ID} \
